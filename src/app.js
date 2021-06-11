936b7ddbe0c56f795dfc58681152ba05b50be50a
aditya101rxl <aditya101rxl@gmail.com>
2021-06-11 14:24:41 +0530

    change in structure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const path = require("path");
 // require mongobdb 15:00 - 20:00
+const hbs = require("hbs");
 const app = express();
 const PORT = process.env.PORT || 8001
 
 
 // middleware and setting paths
-app.use(express.static(path.join(__dirname, "../public")));
+const public_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+app.use(express.static(public_path));
 app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
 
 // routing
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     // res.send("Hello, I'm there");
     res.render("index");
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 })
