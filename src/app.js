1a20cda886def09d7d9086c3fcef45f64a044321
aditya101rxl <aditya101rxl@gmail.com>
2021-06-17 04:58:06 +0530

    backend update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
-// require mongobdb 15:00 - 20:00
+const private = require("./private");
+// require("./db/connect");
 const hbs = require("hbs");
+const eventModel = require("./models/event");
 const app = express();
 const PORT = process.env.PORT || 8001
 
@@ -15,10 +17,54 @@ app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
+// console.log(private.CONNECTION_URL);
 // routing
 app.get("/", (req, res) => {
     // res.send("Hello, I'm there");
     res.render("index");
+})
+app.get("/gallery", (req, res) => {
+    // res.send("Hello, Welcome to Gallery");
+    res.render("gallery");
+})
+app.get("/add-event", (req, res) => {
+    // res.send("Hello, Welcome Admin");
+    res.render("add_event");
+})
+app.post("/add-event", async(req, res) => {
+    console.log(req.body);
+    // try {
+    //     console.log(req.body);
+    //     console.log(private.PASSWORD);
+    //     const pass = req.body.password;
+    //     if (pass === private.PASSWORD) {
+    //         const event = new eventModel({
+    //             heading: req.body.heading,
+    //             date: req.body.date,
+    //             details: req.body.details,
+    //         });
+    //         event.save()
+    //             .then(() => {
+    //                 res.redirect("/");
+    //             })
+    //             .catch(err => {
+    //                 // res.redirect("/");
+    //                 alert("Some error occured! try again");
+    //             })
+    //     } else {
+    //         alert("Wrong Password!!! Try Again.");
+    //     }
+    // } catch (error) {
+    //     res.status(400).send(error);
+    // }
+})
+app.get("/add-photo", (req, res) => {
+    // res.send("Hello, Welcome Admin");
+    res.render("add-photo");
+})
+app.post("/add-photo", (req, res) => {
+    console.log(private.PASSWORD);
+    console.log("add-photo");
 })
 
 app.listen(PORT, () => {
