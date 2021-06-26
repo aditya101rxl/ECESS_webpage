4841480b0167f972e06a6e8ac66564a2eca42356
aditya101rxl <aditya101rxl@gmail.com>
2021-06-26 14:35:25 +0530

    filling data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
-const private = require("./private");
+// const bodyParser = require('body-parser');
+// const private = require("./private");
 // require("./db/connect");
 const hbs = require("hbs");
-const eventModel = require("./models/event");
+// const eventModel = require("./models/event");
 const app = express();
 const PORT = process.env.PORT || 8001
 
@@ -16,56 +17,59 @@ app.use(express.static(public_path));
 app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// console.log(private.CONNECTION_URL);
 // routing
+
+// home page router
 app.get("/", (req, res) => {
-    // res.send("Hello, I'm there");
-    res.render("index");
-})
+        res.render("index");
+    })
+    // render gallery page
 app.get("/gallery", (req, res) => {
-    // res.send("Hello, Welcome to Gallery");
     res.render("gallery");
 })
+
+// backend updates
+/*
+
+
+// fetching new events from DB
 app.get("/add-event", (req, res) => {
-    // res.send("Hello, Welcome Admin");
     res.render("add_event");
 })
+
+// adding new events to DB
 app.post("/add-event", async(req, res) => {
     console.log(req.body);
-    // try {
-    //     console.log(req.body);
-    //     console.log(private.PASSWORD);
-    //     const pass = req.body.password;
-    //     if (pass === private.PASSWORD) {
-    //         const event = new eventModel({
-    //             heading: req.body.heading,
-    //             date: req.body.date,
-    //             details: req.body.details,
-    //         });
-    //         event.save()
-    //             .then(() => {
-    //                 res.redirect("/");
-    //             })
-    //             .catch(err => {
-    //                 // res.redirect("/");
-    //                 alert("Some error occured! try again");
-    //             })
-    //     } else {
-    //         alert("Wrong Password!!! Try Again.");
-    //     }
-    // } catch (error) {
-    //     res.status(400).send(error);
-    // }
+    try {
+        console.log(req.body);
+        const pass = req.body.password;
+        if (pass === private.PASSWORD) {
+            res.send(req.body);
+            // save events to database
+
+        } else {
+            res.render("add_event", { status: 'alert-danger', message: "wrong password!!!... Try Again" });
+        }
+    } catch (error) {
+        res.status(400).send(error);
+    }
 })
+
+// fatching photos from DB
 app.get("/add-photo", (req, res) => {
-    // res.send("Hello, Welcome Admin");
     res.render("add-photo");
 })
+
+// adding new photos to DB
 app.post("/add-photo", (req, res) => {
     console.log(private.PASSWORD);
     console.log("add-photo");
 })
+
+
+*/
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
